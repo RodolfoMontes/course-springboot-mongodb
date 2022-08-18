5a1ccf9bfff085c872ac8374eeb063dc0bece18c
Rodolfo Montes <montes.rodolforodrigues@gmail.com>
2022-08-18 14:50:27 -0300

    Project created

---
+++ src/main/java/com/montes/courseworkshopmongo/CourseWorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.montes.courseworkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montes/courseworkshopmongo/CourseWorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montes.courseworkshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
