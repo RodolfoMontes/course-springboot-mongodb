23720af1d7569a5c375651da975a8c5e7ed94f50
Rodolfo Montes <montes.rodolforodrigues@gmail.com>
2022-08-18 20:39:05 -0300

    Obtendo um usuario por id

--- src/main/java/com/montes/courseworkshopmongo/services/UserService.java
+++ src/main/java/com/montes/courseworkshopmongo/services/UserService.java
@@ -1,21 +1,27 @@
 package com.montes.courseworkshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.montes.courseworkshopmongo.domain.User;
 import com.montes.courseworkshopmongo.repository.UserRepository;
-
+import com.montes.courseworkshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
